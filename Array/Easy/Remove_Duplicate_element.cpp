5a8b41e2b9f712019776a7decf176dedc998136d
rohit <rohitpannalalverma@gmail.com>
2025-07-25 01:43:59 +0530

    New Commit

--- Array/Easy/Remove_Duplicate_element.cpp
+++ Array/Easy/Remove_Duplicate_element.cpp
@@ -7,20 +7,35 @@ class Solution {
     int Remove_Duplicate_Element(vector<int> &nums) {
       set<int> s(nums.begin(), nums.end());
       int count = 0;
-      for(auto it : nums){
+      for(auto it : s){
          nums[count++] = it;
       }
 
       return count;
 
     }
+
+    // 2 .Optimal_apporach
+    int Remove_Duplicate_Element1(vector<int> &nums) {
+        int count = 1;
+        for(int i = 0; i<nums.size()-1; i++){
+            if(nums[i] != nums[i+1]){
+                nums[count++] = nums[i+1];
+            }
+
+        }
+        return count;
+    }
+
+
 };
 
 int main() {
     Solution s;
     vector<int> v  = {1,1,2,2,2,3,3,3};
-    int n = s.Remove_Duplicate_Element(v);
-    for(int i = 0; i<n; i++){
+    // int n = s.Remove_Duplicate_Element(v);
+    int n1 = s.Remove_Duplicate_Element1(v);
+    for(int i = 0; i<n1; i++){
         cout << v[i];
     }
     
