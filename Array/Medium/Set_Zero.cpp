ce4ec4f2d37b8475c5caccad462e3c990312f12c
rohit <rohitpannalalverma@gmail.com>
2025-09-01 22:33:19 +0530

    Daily_leetcode

---
+++ Array/Medium/Set_Zero.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void setMatrixZeroes(vector<vector<int>> &mat)
+    {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<int> rows(n, 0);
+        vector<int> cols(m, 0);
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (mat[i][j] == 0)
+                {
+                    rows[i] = 1;
+                    cols[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (mat[i][j] != 0)
+                {
+                    if (rows[i] || cols[j])
+                    {
+                        mat[i][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+   
+    void setMatrixZeroes_optimal(vector<vector<int>> &mat) {
+      int n = mat.size();
+      int m = mat[0].size();
+      vector<int> rows(n,0);
+      vector<int> cols(m,0);
+      
+      for(int i = 0; i<n; i++){
+          for(int j = 0; j<m; j++){
+              if(mat[i][j] == 0){
+                  rows[i] = 1;
+                  cols[j] = 1;
+              }
+          }
+      }
+         
+      for(int i = 0; i<n; i++){
+          for(int j =0; j<m; j++){
+              if(mat[i][j] != 0){
+                  if(rows[i] || cols[j]){
+                      mat[i][j] = 0;
+                  }
+              }
+          }
+      }     
+    }
+};
+
+
+int main()
+{
+    Solution s;
+}
